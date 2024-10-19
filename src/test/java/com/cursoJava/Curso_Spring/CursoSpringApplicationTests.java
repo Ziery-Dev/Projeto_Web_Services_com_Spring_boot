c0dfb69a4fe33814fb61cc31a38297481f75278c
Ziery-Dev <zieryDev@gmail.com>
2024-10-19 13:41:10 -0300

    Projeto criado

---
+++ src/test/java/com/cursoJava/Curso_Spring/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoJava.Curso_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
